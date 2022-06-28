52da7937dbe7d2a6cfca88f87878dcc278f9d60b
vfastrez <vincent.fastrez@gmail.com>
2022-06-28 12:58:05 +0200

    ADD: CarouselComps.vue, missing database get data

--- src/components/CarouselComps.vue
+++ src/components/CarouselComps.vue
@@ -1,61 +1,106 @@
 <template>
-    <Carousel :itemsToShow="3.95" :wrapAround="true">
+  <div v-for="data in datascarousels"
+       :key="data.indexNbx">
+    <div class="text-h3">{{data.type}}</div>
+    <Carousel :itemsToShow="3" :wrapAround="true">
 
-    <Slide v-for="data in datascarousels"
-           :key="data.indexNbx"
-           :name="data.indexNbx"
-           class="q-pa-sm">
-      <div class="full-height full-width flex flex-center" style="background-color: rgba(0, 0, 0, 0.68) !important;">
-        <div class="custom-caption">
-          <div class="text-h2 main_line animation_2">{{ title }}</div><br><br><br>
-          <div class="text-h4 animation_1">{{ description }}</div><br><br><br>
-          <div class="animation_2">
-            <q-btn size="md" :style="'background:#ff851b; color: white'" label="ORDER" :href="link"/>
+      <Slide v-for="dataZoom in data.restauData"
+             :key="dataZoom.indexNbx"
+             :name="dataZoom.indexNbx"
+             :url="dataZoom.picture"
+             class="q-pa-sm">
+        <div class="full-height full-width flex flex-center" style="background-color: rgba(0, 0, 0, 0.68) !important;">
+          <div class="custom-caption">
+            <div class="text-h4 font-size12 main_line animation_2">{{ dataZoom.title }}</div><br><br><br>
+            <div class="text-h5 animation_1">{{ dataZoom.description }}</div><br><br><br>
+            <div class="animation_2">
+              <q-btn size="md" :style="'background:#ff851b; color: white'" label="ORDER" :href="dataZoom.link"/>
+            </div>
           </div>
         </div>
-      </div>
-    </Slide>
+      </Slide>
 
-    <template #addons>
-      <navigation />
-      <pagination />
-    </template>
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
 
-  </Carousel>
+    </Carousel>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { Carousel, Pagination, Slide, Navigation } from 'vue3-carousel'
+import { Carousel, Pagination, Slide, Navigation } from 'vue3-carousel';
+
+import 'vue3-carousel/dist/carousel.css';
+
+const datasCarousel=[
+  {
+    type: 'Italian',
+    indexNbx: 3,
+    restauData : [
+      {
+      title: 'Idiot',
+      indexNbx: 3,
+      icon: 'school',
+      link: 'https://quasar.dev',
+      picture: '~assets/openfood_logo.svg',
+      description: 'school',
+      },
+      {
+        title: 'Idiot2',
+        indexNbx: 3,
+        icon: 'school',
+        link: 'https://quasar.dev',
+        picture: '~assets/openfood_logo.svg',
+        description: 'school',
+      },
+      {
+        title: 'Idiot3',
+        indexNbx: 3,
+        icon: 'school',
+        link: 'https://quasar.dev',
+        picture: '~assets/openfood_logo.svg',
+        description: 'school',
+      },
+    ],
+  },
+  {
+    type: 'Chinese',
+    indexNbx: 3,
+    restauData : [
+      {
+        title: 'Idiot',
+        indexNbx: 3,
+        icon: 'school',
+        link: 'https://quasar.dev',
+        picture: '~assets/openfood_logo.svg',
+        description: 'school',
+      },
+      {
+        title: 'Idiot2',
+        indexNbx: 3,
+        icon: 'school',
+        link: 'https://quasar.dev',
+        picture: '~assets/openfood_logo.svg',
+        description: 'school',
+      },
+      {
+        title: 'Idiot3',
+        indexNbx: 3,
+        icon: 'school',
+        link: 'https://quasar.dev',
+        picture: '~assets/openfood_logo.svg',
+        description: 'school',
+      },
+    ],
+  }
+]
+
 
 export default defineComponent({
-  name: 'CarouselComps',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    nameGl: {
-      type: String,
-      default: '#'
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    picture: {
-      type: String,
-      default: ''
-    },
-
-    description :{
-      type: String,
-      default: ''
-    }
-  },
+  name: 'CarrouselComps',
 
   components: {
     Carousel,
@@ -64,9 +109,45 @@ export default defineComponent({
     Pagination,
     Navigation,
   },
-})
+
+  setup () {
+    return {
+      datascarousels: datasCarousel,
+    }
+  },
+
+  data()
+  {
+    return{
+      carousel: 1
+    }
+  }
+});
 </script>
 
 <style scoped>
+.carousel__slide > .carousel__item {
+  transform: scale(1);
+  opacity: 0.5;
+  transition: 0.5s;
+}
+.carousel__slide--visible > .carousel__item {
+  opacity: 1;
+  transform: rotateY(0);
+}
+.carousel__slide--next > .carousel__item {
+  transform: scale(0.9) translate(-10px);
+}
+.carousel__slide--prev > .carousel__item {
+  transform: scale(0.9) translate(10px);
+}
+.carousel__slide--active > .carousel__item {
+  transform: scale(1.1);
+}
 
+.bg-image {
+  background-image: url("~assets/openfood_logo.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,84 +1,19 @@
 <template>
-  <q-page>
-    <q-carousel
-      animated
-      v-model="carousel"
-      arrows
-      swipeable
-      navigation
-      infinite
-      autoplay
-      control-color="white"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      height="639px"
-    >
-      <q-carousel-slide
-        v-for="data in datascarousels"
-        :key="data.indexNbx"
-        :name="data.indexNbx" >
-
-        <div class="full-height full-width flex flex-center" style="background-color: rgba(0, 0, 0, 0.68) !important;">
-          <div class="custom-caption">
-            <div class="text-h2 main_line animation_2">{{ data.title }}</div><br><br><br>
-            <div class="text-h4 animation_1">{{ data.description }}</div><br><br><br>
-            <div class="animation_2">
-              <q-btn size="md" :style="'background:#ff851b; color: white'" label="ORDER" :href="data.link"/>
-            </div>
-          </div>
-        </div>
-      </q-carousel-slide>
-
-    </q-carousel>
+  <q-page padding class="bg-image">
+    <carousel-comps></carousel-comps>
   </q-page>
 </template>
-
 <script>
-import { defineComponent } from 'vue';
-
-const datasCarousel=[
-  {
-    title: 'Docs',
-    indexNbx: 1,
-    icon: 'school',
-    link: 'https://quasar.dev',
-    picture: '~assets/openfood_logo.svg',
-    description: 'school',
-  },
-  {
-    title: 'Doc5s',
-    indexNbx: 2,
-    icon: 'school',
-    link: 'https://quasar.dev',
-    picture: '~assets/openfood_logo.svg',
-    description: 'school',
-  },
-  {
-    title: 'Idiot',
-    indexNbx: 3,
-    icon: 'school',
-    link: 'https://quasar.dev',
-    picture: '~assets/openfood_logo.svg',
-    description: 'school',
-  },
-]
+import CarouselComps from "components/CarouselComps.vue"
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage',
-
-  setup () {
-    return {
-      datascarousels: datasCarousel,
-    }
-  },
-
-  data()
-  {
-    return {
-      carousel: 1
-    }
+  components: {
+    CarouselComps
   }
 })
-
-
 </script>
+
+<style scoped>
+
+</style>
