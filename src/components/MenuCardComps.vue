af3bd8aba9c12b64a26ad6be4a3ed7a1701cde0f
vfastrez <vincent.fastrez@gmail.com>
2022-07-09 13:20:54 +0200

    ADD: Read Menu

--- src/components/MenuCardComps.vue
+++ src/components/MenuCardComps.vue
@@ -16,19 +16,88 @@
           @click="deleteMenu(menuId)"
         />
         <q-btn flat round color="blue" icon="edit" />
-        <q-btn flat round color="green" icon="library_books" />
+        <q-btn
+          flat
+          round
+          color="green"
+          icon="library_books"
+          @click="printMenu = true"
+        />
       </q-card-actions>
     </q-card-section>
   </q-card>
+  <div>
+    <q-dialog v-model="printMenu">
+      <q-card>
+        <q-card-section class="column items-center">
+          <q-input
+            filled
+            v-model="menusArray[0].name"
+            label="Menu name"
+            readonly
+          />
+          <q-input
+            filled
+            v-model="menusArray[0].details"
+            label="Menu details"
+            readonly
+          />
+          <q-input
+            filled
+            v-model="menusArray[0].price"
+            label="Menu price"
+            readonly
+          />
+          <q-input
+            filled
+            v-model="menusArray[0].preparationTime"
+            label="Menu preparation"
+            readonly
+          />
+          <p style="font-size: 3em; margin-top: 10%">Articles</p>
+          <div v-for="art in menusArray[0].article" :key="art">
+            <p style="font-size: 2em">{{ art.name }}</p>
+            <q-input
+              filled
+              v-model="art.price"
+              label="Article price"
+              readonly
+            />
+            <q-input
+              filled
+              v-model="art.quantity"
+              label="Article quantity"
+              readonly
+            />
+            <q-input
+              filled
+              v-model="art.timePreparedDef"
+              label="Article preparation"
+              readonly
+            />
+          </div>
+        </q-card-section>
+        <q-card-actions align="right"> </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
 import { api } from "src/boot/axios";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { Cookies } from "quasar";
 
 export default defineComponent({
   name: "menu-card-comps",
+  inheritAttrs: false,
   props: ["menuName", "menuId"],
+
+  data() {
+    return {
+      menusArray: [],
+    };
+  },
 
   methods: {
     async deleteMenu(idMenu) {
@@ -36,6 +105,44 @@ export default defineComponent({
         location.reload();
       });
     },
+  },
+
+  async mounted() {
+    const restauID = Cookies.get("restau_id");
+
+    this.menus = await api
+      .get("restaurants/" + restauID)
+      .then(async (response) => {
+        for (let i = 0; i < response.data.length; i++) {
+          for (let j = 0; j < response.data[i].menus.length; j++) {
+            if (response.data[i].menus[j].id === this.menuId) {
+              const articlesList = await api
+                .get("restaurants/menu/" + this.menuId)
+                .then(async (res) => {
+                  return res.data[0].articles;
+                });
+              this.menusArray.push({
+                name: response.data[i].menus[j].name,
+                details: response.data[i].menus[j].details,
+                price: response.data[i].menus[j].price,
+                preparationTime: response.data[i].menus[j].timePreparedDef,
+                article: articlesList,
+              });
+            }
+          }
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    console.log(this.menusArray);
+  },
+
+  setup() {
+    return {
+      printMenu: ref(null),
+    };
   },
 });
 </script>
